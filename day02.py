b46249f9130c707d9de3e9a4c8b21cffa19fd583
mkpawlowska <mkpawlowska@wp.pl>
2019-10-22 20:27:47 +0200

    Przykłady z zajec

---
+++ day02.py
@@ -0,0 +1,17 @@
+# Tytuł: "Ala ma kota"
+
+tekst = "Tytuł: \"Ala ma kota\""
+tekst = '''Tytuł: "Ala ma kota" test'''
+print(tekst.)
+
+#ulamek = 1.34
+#ilosc_powtorzen = 4
+
+#x = input("Podaj pierwszą liczbę: ")
+#y = input("Podaj drugą liczbę: ")
+
+#suma = int(x) + int(y)
+
+# exit()
+# comments
+#print(suma)
